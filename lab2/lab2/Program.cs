4864f26952d2bd8ce896e756ed07dbd6974102a5
ann.kurenna <anna.kurennaya38@gmail.com>
2020-02-18 16:05:39 +0200

    split string into elements

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -10,6 +10,7 @@ namespace lab2
     public class Student
     {
         public string NameString { get; set; }
+        public int Mark1, Mark2, Mark3, Mark4, Mark5;
     }
 
 
@@ -22,16 +23,31 @@ namespace lab2
             string pathToFile = dirName + "input.csv";
 
             StreamReader sr = new StreamReader(pathToFile, System.Text.Encoding.Default);
-            int numInFIle = Convert.ToInt32(sr.ReadLine());
-            List<string> students = new List<string>();
+            var line = sr.ReadLine();
+            var values = line.Split(',');
+            int numInFIle = Convert.ToInt32(values[0]);
+            List<Student> students = new List<Student>();
             for (int i = 0; i < numInFIle; i++)
             {
-                students.Add(sr.ReadLine());
+                students.Add(new Student{ NameString = sr.ReadLine() });
+                int index = students.Count - 1;
+                var temp = students[index].NameString.Split(',');
+                if (temp[6] == "FALSE") {
+                    students[index].NameString = temp[0];
+                    students[index].Mark1 = Convert.ToInt32(temp[1]);
+                    students[index].Mark2 = Convert.ToInt32(temp[2]);
+                    students[index].Mark3 = Convert.ToInt32(temp[3]);
+                    students[index].Mark4 = Convert.ToInt32(temp[4]);
+                    students[index].Mark5 = Convert.ToInt32(temp[5]);
+                }
+                else
+                {
+                    students.RemoveRange(students.Count - 1, 1);
+                }
             }
-            Console.WriteLine();
-            for (int i = 0; i < numInFIle; i++)
+            for (int i = 0; i < students.Count; i++)
             {
-                Console.WriteLine(students[i]);
+                Console.WriteLine(students[i].NameString);
             }
 
             Console.ReadKey();
