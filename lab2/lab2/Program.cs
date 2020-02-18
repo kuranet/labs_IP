b3f5aebacf2a8e0774d34aaef5015bb563aa37cb
ann.kurenna <anna.kurennaya38@gmail.com>
2020-02-18 14:46:40 +0200

    reading strings from one file

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -7,15 +7,33 @@ using System.Threading.Tasks;
 
 namespace lab2
 {
+    public class Student
+    {
+        public string NameString { get; set; }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
             string dirName;
-            dirName = Directory.GetCurrentDirectory() + '\\';
-            dirName += Console.ReadLine();
-            bool lab = Directory.Exists(dirName);
-            Console.WriteLine(lab);
+            dirName = Directory.GetCurrentDirectory()+"\\lab2\\";
+            string pathToFile = dirName + "input.csv";
+
+            StreamReader sr = new StreamReader(pathToFile, System.Text.Encoding.Default);
+            int numInFIle = Convert.ToInt32(sr.ReadLine());
+            List<string> students = new List<string>();
+            for (int i = 0; i < numInFIle; i++)
+            {
+                students.Add(sr.ReadLine());
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numInFIle; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+
             Console.ReadKey();
         }
     }
